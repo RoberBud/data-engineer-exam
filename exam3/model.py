d2c3930b8843eda0a737b7140db445d097ce15cc
EC2 Default User <ec2-user@ip-172-31-11-236.us-east-2.compute.internal>
2020-07-20 09:29:18 +0000

    [Add] initial commit

--- exam3/model.py
+++ exam3/model.py
@@ -1,5 +1,147 @@
+
+import torch 
 import torch.nn as nn
+import pandas as pd
+import numpy as np
+import ast
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 
 
-class Model(nn.Module):
-    pass
+
+inputs = pd.read_csv('../exam2/result/train.csv',usecols = [1], header = None, skiprows = 1)
+
+
+input_list = []
+convert = {}
+for index, row in inputs.iterrows():
+
+    data = row[1]
+    data = ast.literal_eval(data)
+
+    data = list(map(int, data))
+    convert[index] = data
+
+
+df= pd.DataFrame.from_dict(convert, orient='index')
+df = df.fillna(0)
+
+#df = preprocessing.scale(df)
+
+
+zscore = preprocessing.StandardScaler()
+
+df = zscore.fit_transform(df)
+
+train_df, val_df = train_test_split(
+  df,
+  test_size=0.5,
+  random_state=42
+)
+
+
+
+def create_dataset(df):
+
+  sequences = df.astype(np.float32).tolist()
+  dataset = [torch.tensor(s).unsqueeze(1).float() for s in sequences]
+
+  n_seq, seq_len, n_features = torch.stack(dataset).shape
+
+  return dataset, seq_len, n_features
+
+
+train_dataset, seq_len, n_features = create_dataset(train_df)
+val_dataset, _, _ = create_dataset(val_df)
+
+
+
+
+
+class Encoder(nn.Module):
+
+  def __init__(self, seq_len, n_features, embedding_dim=64):
+  # def __init__(self, seq_len, n_features, embedding_dim=2):
+    super(Encoder, self).__init__()
+
+    self.seq_len, self.n_features = seq_len, n_features
+    self.embedding_dim, self.hidden_dim = embedding_dim, 2 * embedding_dim
+
+    self.rnn1 = nn.LSTM(
+      input_size=n_features,
+      hidden_size=self.hidden_dim,
+      num_layers=1,
+      batch_first=True
+    )
+    
+    self.rnn2 = nn.LSTM(
+      input_size=self.hidden_dim,
+      hidden_size=embedding_dim,
+      num_layers=1,
+      batch_first=True
+    )
+
+  def forward(self, x):
+    x = x.reshape((1, self.seq_len, self.n_features))
+
+    x, (_, _) = self.rnn1(x)
+    x, (hidden_n, _) = self.rnn2(x)
+
+    return hidden_n.reshape((self.n_features, self.embedding_dim))
+
+class Decoder(nn.Module):
+
+  def __init__(self, seq_len, input_dim=64, n_features=1):
+    super(Decoder, self).__init__()
+
+    self.seq_len, self.input_dim = seq_len, input_dim
+    self.hidden_dim, self.n_features = 2 * input_dim, n_features
+
+    self.rnn1 = nn.LSTM(
+      input_size=input_dim,
+      hidden_size=input_dim,
+      num_layers=1,
+      batch_first=True
+    )
+
+    self.rnn2 = nn.LSTM(
+      input_size=input_dim,
+      hidden_size=self.hidden_dim,
+      num_layers=1,
+      batch_first=True
+    )
+
+    self.output_layer = nn.Linear(self.hidden_dim, n_features)
+
+  def forward(self, x):
+    x = x.repeat(self.seq_len, self.n_features)
+    x = x.reshape((self.n_features, self.seq_len, self.input_dim))
+
+    x, (hidden_n, cell_n) = self.rnn1(x)
+    x, (hidden_n, cell_n) = self.rnn2(x)
+    x = x.reshape((self.seq_len, self.hidden_dim))
+
+    return self.output_layer(x)
+
+class RecurrentAutoencoder(nn.Module):
+
+  def __init__(self, seq_len, n_features, embedding_dim=64):
+  #def __init__(self, seq_len, n_features, embedding_dim=2):
+    super(RecurrentAutoencoder, self).__init__()
+
+    self.encoder = Encoder(seq_len, n_features, embedding_dim)
+    self.decoder = Decoder(seq_len, embedding_dim, n_features)
+
+  def forward(self, x):
+    x = self.encoder(x)
+    x = self.decoder(x)
+
+    return x
+
+#model = RecurrentAutoencoder(seq_len, n_features, 6)
+model = RecurrentAutoencoder(seq_len, n_features, 128)
+
+
+
+
--- exam3/train.py
+++ exam3/train.py
@@ -1,5 +1,90 @@
-from model import Model
+import torch 
+import torch.nn as nn
+import numpy as np
+import copy
+import model
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Test for argparse')
+parser.add_argument('--epochs', help='Number of epochs', default=10)
+args = parser.parse_args()
+
+
+epoch = int(args.epochs)
+
+def train_model(model, train_dataset, val_dataset, n_epochs):
+  optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+  criterion = nn.L1Loss(reduction='sum')
+  #criterion = torch.nn.MSELoss(reduction='mean',reduce=True, size_average=True)
+  history = dict(train=[], val=[])
+
+  best_model_wts = copy.deepcopy(model.state_dict())
+  best_loss = 10000.0
+  
+  for epoch in range(1, n_epochs + 1):
+    model = model.train()
+
+    train_losses = []
+    for seq_true in train_dataset:
+      optimizer.zero_grad()
+
+      seq_true = seq_true
+      seq_pred = model(seq_true)
+
+      loss = criterion(seq_pred, seq_true)
+
+      loss.backward()
+      optimizer.step()
+
+      train_losses.append(loss.item())
+
+    val_losses = []
+    model = model.eval()
+    with torch.no_grad():
+      for seq_true in val_dataset:
+
+        seq_true = seq_true
+        seq_pred = model(seq_true)
+
+        loss = criterion(seq_pred, seq_true)
+        val_losses.append(loss.item())
+
+    train_loss = np.mean(train_losses)
+    val_loss = np.mean(val_losses)
+
+    history['train'].append(train_loss)
+    history['val'].append(val_loss)
+
+    if val_loss < best_loss:
+      best_loss = val_loss
+      best_model_wts = copy.deepcopy(model.state_dict())
+    
+
+    print(f'Epoch {epoch}: train loss {train_loss} val loss {val_loss}')
+
+  model.load_state_dict(best_model_wts)
+  return model.eval(), history
+
+
 
 if __name__ == '__main__':
-    model = Model()
-    pass
+
+
+    model, history = train_model(
+      model.model, 
+      model.train_dataset, 
+      model.val_dataset, 
+      n_epochs=epoch
+    )
+
+
+    if not os.path.isdir("./model"):
+        os.mkdir("./model")
+
+    MODEL_PATH = './model/model.pth'
+    torch.save(model, MODEL_PATH)
+
+
+
+
