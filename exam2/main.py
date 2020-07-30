1369ac4d10d6754bc8d026a4ffccc677ce44df0f
EC2 Default User <ec2-user@ip-172-31-11-236.us-east-2.compute.internal>
2020-07-30 06:20:46 +0000

    [Update] improve the performance of main.py

--- exam2/main.py
+++ exam2/main.py
@@ -13,28 +13,15 @@ def base_reader(file):
     test_data = pd.read_csv(f'./data/{file}_need_aggregate.csv')
     test_data['datetime'] = pd.to_datetime(test_data['datetime'])
     test_data.index = pd.to_datetime(test_data['datetime'])
+    
     min_index = test_data.index.to_period('T')
-    test_data.set_index(min_index, inplace =True)
-    
-    drop = test_data[~test_data.index.duplicated()]
-    drop = drop.index
-    
-    drop2 = drop.to_timestamp()
-    column = [[] for i in range(len(drop2))]
-    new = pd.Series(column, index=drop2)
-    
-    
-    for index, row in test_data.iterrows():
-        index_time = index.to_timestamp()
-        res = new.loc[index_time]
-        
-        res.append(row[1])
-        
-        new.loc[index_time] = res 
-        
+    test_data.index =  min_index
    
+    test_data.rename(columns={'datetime':'temp'})
     
-    new.to_csv(f'./result/{file}.csv',header = 0)
+    new = test_data['EventId'].groupby('datetime').agg(list)
+  
+    new.to_csv(f'./result/{file}.csv')
     
 base_reader('train')
 base_reader('test')
