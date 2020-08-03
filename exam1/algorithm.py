36d1617676778e109ea765fdd2a0934536caed6d
EC2 Default User <ec2-user@ip-172-31-11-236.us-east-2.compute.internal>
2020-08-03 04:32:08 +0000

    [Update] modified the algorithm.py to online version

--- exam1/algorithm.py
+++ exam1/algorithm.py
@@ -1,51 +1,24 @@
-import pandas as pd 
-from sklearn.cluster import DBSCAN
-from sklearn import preprocessing
- 
+from creme import anomaly
+from creme import compose
+from creme import preprocessing
 
-class Detector():
-    
-    def data_preprocessing (self, path):
+class Detector:
+    def __init__(self):
         
-        self.df = pd.read_csv(f'{path}')
+        self.model = anomaly.HalfSpaceTrees(n_trees = 10 , height = 3 ,window_size= 25, seed=42,limits = {'ptr':[-15,32]})
+            
+        self.scaler = preprocessing.StandardScaler()
         
-        self.data = self.df[['timestamp','value']]
+    def fit_predict(self, ptr):
+        features = {'ptr': float(ptr)}
+        features = self.scaler.fit_one(features).transform_one(features)
         
-        self.df = preprocessing.scale(self.data)
-        
-        self.df = pd.DataFrame(self.df)
-        self.df.columns = ['timestamp', 'value']
-        
-        return self.df
-
-    def DBScan_processing (self):
-
-        outlier_detection = DBSCAN( eps = 1.15, metric="euclidean", min_samples = 3, n_jobs = 1) 
-        clusters = outlier_detection.fit_predict(self.df)
-
-        return clusters
-        
-        
-    def determine(self, clusters):
-        self.df['anomaly3'] = pd.Series(clusters)
-        
-        Anomaly = self.df[(self.df.anomaly3 == -1)].index.tolist()
-        
-        for i in Anomaly:
-            self.df.iloc[i, 2] = 1
-        
-        return self.df['anomaly3'].tolist()
-    
-    def fit_predict(self, path):
-        self.data_preprocessing(path)
-        clusters = self.DBScan_processing()
-        result = self.determine(clusters)
-        return result
+        self.model = self.model.fit_one(features)
        
-
-
-
-
-
-
-
+        if self.model.score_one(features) >= 0.5:
+            pred = 1
+        else:
+            pred = 0
+        
+       
+        return pred
